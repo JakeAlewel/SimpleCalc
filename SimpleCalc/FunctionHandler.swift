d0e259af209e2a0e4f0ffe33ac8fb4139044d3f8
Jacob Alewel <jakealewel@ymail.com>
2015-10-08 09:28:42 -0700

    Updated FunctionHandler to calculate the count of inputs, and the factorial of a single input with proper error messages

--- SimpleCalc/FunctionHandler.swift
+++ SimpleCalc/FunctionHandler.swift
@@ -19,6 +19,43 @@ func functionForInputString(inputString: String) -> String? {
     return nil;
 }
 
+func calculateAndPrintAverageForNumbers(components: Array<Int>) {
+    
+}
+
+func calculateAndPrintFactorialForNumbers(components: Array<Int>) {
+    if components.count == 1 {
+        let number = components[0];
+        if number == 0 {
+            printResult(1);
+        } else {
+            var factorial = 1;
+            for index in 1...number {
+                factorial *= index;
+            }
+            printResult(factorial);
+        }
+    } else {
+        print("Factorial Requires Single Value Input");
+    }
+}
+
 func handleFunction(inputString:String, functionString:String) {
-    print("Found operation " + functionString)
+    let components = arrayOfIntsForInputString(inputString);
+    if components == nil {
+        print("Invalid Value Input(s)");
+        return;
+    }
+    
+    switch functionString {
+        case "count":
+            printResult(components!.count);
+        case "avg":
+            calculateAndPrintAverageForNumbers(components!);
+        case "fact":
+            calculateAndPrintFactorialForNumbers(components!)
+        
+        default:
+            print("Invalid Function");
+    }
 }
